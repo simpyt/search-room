d39ac250677476d08bb6e77a3edc9c2a3cf70e57
Simon Pythoud <simon.pythoud@swissmarketplace.group>
2025-12-04 19:29:08 +0100

    feat: add Homegate/ImmoScout24 CDN domains to next/image config

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,32 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "images.unsplash.com",
       },
+      // Homegate CDN domains for listing images
+      {
+        protocol: "https",
+        hostname: "s.homegate.ch",
+      },
+      {
+        protocol: "https",
+        hostname: "*.homegate.ch",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.homegate.ch",
+      },
+      {
+        protocol: "https",
+        hostname: "images.homegate.ch",
+      },
+      // ImmoScout24 (same company, shared CDN)
+      {
+        protocol: "https",
+        hostname: "*.immoscout24.ch",
+      },
+      {
+        protocol: "https",
+        hostname: "s.immoscout24.ch",
+      },
     ],
   },
 };
